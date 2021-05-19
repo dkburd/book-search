79f6b0ea9a8ff155068a3998b74e428c1df3cff7
Dana Burd <burd.dana@gmail.com>
2021-05-19 10:35:02 -0700

    adj

--- src/SearchArea.js
+++ src/SearchArea.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 
+
 export default function SearchArea() {
+
+//make it the value of the input text....
+const searchTerm='alien'
+// const searchTerm = #searchInput.val()
+
+// getBooks=(e) => {
+// preventDefault()
+// console.log('fetch will be here')
+//   }
+
+// for submit not working onClick={this.getBooks}
   return (
     <div style={SearchStyle}>
         <form>
         <label for="SearchInput">Search: </label>
         <input type="search" id="searchInput" key='searchInput' name="search-input"></input>
-        <input type="submit" value="Search"></input>
+        <input type="submit" value="Search" ></input>
         </form>
     </div>
   )
